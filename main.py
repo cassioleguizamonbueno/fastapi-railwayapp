3db03b0ff5ca8cc8386053d45164a6a9613bfe4d
cassioleguizamonbueno <36665958+cassioleguizamonbueno@users.noreply.github.com>
2025-10-23 13:05:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+@app.get("/teste")
+async def root():
+    return {"greeting": "teste!", "message": "teste to FastAPI!"}
